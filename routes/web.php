e80e1cce52e8dc1ee57ff072c59f1ee8e8cdef97
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-10 07:58:36 +0800

    Group routes to apply name prefix

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,35 @@ use App\Classes\ChallengeGenerator;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::get('/game', function (Request $request, ChallengeGenerator $challengeGenerator){
-    if($request->session()->exists('game')){
+Route::name("game.")->group(function (){
+    Route::get('/game', function (Request $request, ChallengeGenerator $challengeGenerator){
+        if($request->session()->exists('game')){
+            $game = $request->session()->get('game');
+        }else{
+            $game = $challengeGenerator->generate();
+            $request->session()->put('game', $game);
+        }
+
+        if($game->isOver()){
+            $request->session()->forget('game');
+        }
+
+        $disabledKeys = $game->isOver() ? true : $game->getUsedLetters();
+        
+        return view('game.show', compact('game', 'disabledKeys'));
+    })->name('show');
+
+    Route::put('/game', function (Request $request) {
         $game = $request->session()->get('game');
-    }else{
-        $game = $challengeGenerator->generate();
-        $request->session()->put('game', $game);
-    }
 
-    if($game->isOver()){
-        $request->session()->forget('game');
-    }
+        $skip = $request->input('skip', false);
+        if($skip){
+            $game->skip();
+        }else{
+            $guess = $request->input('guess');
+            $game->guess($guess);
+        }
 
-    $disabledKeys = $game->isOver() ? true : $game->getUsedLetters();
-    
-    return view('game.show', compact('game', 'disabledKeys'));
-})->name('game.show');
-
-Route::put('/game', function (Request $request) {
-    $game = $request->session()->get('game');
-
-    $skip = $request->input('skip', false);
-    if($skip){
-        $game->skip();
-    }else{
-        $guess = $request->input('guess');
-        $game->guess($guess);
-    }
-
-    return redirect()->route('game.show');
-})->name('game.update');
+        return redirect()->route('game.show');
+    })->name('update');
+});
