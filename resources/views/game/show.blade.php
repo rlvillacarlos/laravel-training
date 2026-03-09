f68097ba74bcc2e1cff154b6eca9641d5b5d07e8
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-10 07:50:59 +0800

    Use app and keyboard components

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -1,40 +1,41 @@
-@php
-    $keygroups = [
-        ['Q','W','E','R','T','Y','U','I','O','P'],
-        ['A','S','D','F','G','H','J','K','L'],
-        ['Z','X','C','V','B','N','M'],
-    ];
-@endphp
+<x-app>
+    <x-slot:title>
+        {{ ucwords(str_replace('_',' ',$game->category)) }}
+    </x-slot:title>
+    
 
-@if($game->isCompleted())
-    <div>Congratulations!</div>
-@elseif ($game->isFailed())
-    <div>You failed! The word we are looking for is {{ $game->word }}</div>
-@endif
+    @if($game->isCompleted())
+        <div>Congratulations!</div>
+    @elseif ($game->isFailed())
+        <div>You failed! The word we are looking for is {{ $game->word }}</div>
+    @endif
 
-<div>
-    Category: {{ ucwords(str_replace('_',' ',$game->category)) }}
-</div>
-<div>
-    Remaining Lives: {{ $game->lives }}
-</div>
-<div>
-    {{ $game }}
-</div>
-<br />
-<div>
-    <form method="post" action="{{ route('game.update') }}">
-        @method('put')
-        @csrf
+    <div>
+        Category: {{ ucwords(str_replace('_',' ',$game->category)) }}
+    </div>
+    <div>
+        Remaining Lives: {{ $game->lives }}
+    </div>
+    <div>
+        {{ $game }}
+    </div>
+    <br />
+    <div>
+        <form method="post" action="{{ route('game.update') }}">
+            @method('put')
+            @csrf
 
-        @foreach($keygroups as $keys)
+            <x-keyboard :disabled-keys="$disabledKeys"/>
+
+            @if(!$game->isOver())
             <div>
-                @foreach ($keys as $key)
-                    <button type="submit" name="guess" value="{{ $key }}">
-                        {{ $key }}
-                    </button>
-                @endforeach
+                <button type="submit" name="skip" value=true>
+                    Skip Challenge
+                </button>
             </div>
-        @endforeach
-    </form>
-</div>
+            @else
+                <a href="{{ route('game.show') }}">Next Challenge</a>
+            @endif
+        </form>
+    </div>
+</x-app>
