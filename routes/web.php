d5344277166dc43c6ebef5bce9f143628d02262e
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-10 11:54:52 +0800

    Remove unnecessary views and routes

--- routes/web.php
+++ routes/web.php
@@ -8,36 +8,3 @@ use Illuminate\Http\Request;
 Route::resource('games', GameController::class)->except([
     'edit'
 ])->parameter('games', 'id');
-
-Route::name("game.")->group(function (){
-    Route::get('/game', function (Request $request, ChallengeGenerator $challengeGenerator){
-        if($request->session()->exists('game')){
-            $game = $request->session()->get('game');
-        }else{
-            $game = $challengeGenerator->generate();
-            $request->session()->put('game', $game);
-        }
-
-        if($game->isOver()){
-            $request->session()->forget('game');
-        }
-
-        $disabledKeys = $game->isOver() ? true : $game->getGuesses();
-        
-        return view('game.show', compact('game', 'disabledKeys'));
-    })->name('show');
-
-    Route::put('/game', function (Request $request) {
-        $game = $request->session()->get('game');
-
-        $skip = $request->input('skip', false);
-        if($skip){
-            $game->skip();
-        }else{
-            $guess = $request->input('guess');
-            $game->guess($guess);
-        }
-
-        return redirect()->route('game.show');
-    })->name('update');
-});
