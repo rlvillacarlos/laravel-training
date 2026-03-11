3a9d2cd0fb6de8f7da08ee41335b2c5faccd9c20
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-11 12:25:50 +0800

    Update routes and middlewares for guest and authenrticated users

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,26 @@
 <?php
 
 use App\Classes\ChallengeGenerator;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
-Route::resource('games', GameController::class)->except([
-    'edit'
-])->parameter('games', 'id');
+Route::middleware('guest')->group(function (){
+    Route::name('registration.')->group(function () {
+        Route::get('/registration', [RegistrationController::class,'show'])->name('show');
+        Route::post('/registration', [RegistrationController::class,'save'])->name('save');    
+    });
+
+    Route::get('/', [AuthController::class, 'show'])->name('login');
+    Route::post('/', [AuthController::class, 'login'])->name('auth.login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+        
+    Route::resource('games', GameController::class)->except([
+        'edit'
+    ])
+    ->parameter('games', 'id');
+});
