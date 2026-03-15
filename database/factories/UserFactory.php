8cbae7d56f6891607074353308d915fe4cf9b8b4
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-15 14:33:55 +0800

    Replace name by username

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => Str::of(fake()->unique()->userName())->replace('.', '_'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
