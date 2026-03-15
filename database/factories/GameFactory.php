b04a5af26bdf46ddab34873674fba293f186954f
Russel L. Villacarlos <rlvillacarlos@cvsu.edu.ph>
2026-03-15 14:34:11 +0800

    Implement GameFactory

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -4,12 +4,18 @@ namespace Database\Factories;
 
 use App\Models\Game;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Game>
  */
 class GameFactory extends Factory
 {
+    /**
+     * The starting number of lives being used by the factory.
+     */
+    protected static int $lives = 6;
+
     /**
      * Define the model's default state.
      *
@@ -18,7 +24,9 @@ class GameFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id' => Str::uuid7()->toString(),
+            'name' => 'Game '.fake()->unique()->numberBetween(1),
+            'starting_lives' => static::$lives,
         ];
     }
 }
